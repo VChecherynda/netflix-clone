3561146cec0f006fc7bd23513eee81438b39c074
“Vadym <checherinda.vadym@gmail.com>
2018-11-02 14:51:21 +0200

    Add actionTypes and action for signIn and signUp

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import createSagaMiddleware from 'redux-saga';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -12,7 +12,10 @@ import { watchAuth } from './store/sagas';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 const rootReducer = combineReducers({
   auth: authReducer
@@ -20,9 +23,10 @@ const rootReducer = combineReducers({
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, composeEnhancers(
-  applyMiddleware(sagaMiddleware)
-));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
 
 sagaMiddleware.run(watchAuth);
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,7 +1,43 @@
 import * as actionTypes from './actionTypes';
 
-export const authStart = () => {
+export const signInStart = () => {
   return {
-      type: actionTypes.AUTH_SIGNUP_START
+      type: actionTypes.SIGNIN_START
+  };
+};
+
+export const signInSuccess = (token, userId) => {
+  return {
+      type: actionTypes.SIGNIN_SUCCESS,
+      userId: userId,
+      token: token
+  };
+};
+
+export const signInFail = error => {
+  return {
+      type: actionTypes.SIGNIN_SUCCESS,
+      error: error
+  };
+};
+
+export const signUpStart = () => {
+  return {
+      type: actionTypes.SIGNUP_START
+  };
+};
+
+export const signUpSuccess = (token, userId) => {
+  return {
+      type: actionTypes.SIGNUP_SUCCESS,
+      userId: userId,
+      token: token
+  };
+};
+
+export const signUpFail = error => {
+  return {
+      type: actionTypes.SIGNUP_FAIL,
+      error: error
   };
 };
